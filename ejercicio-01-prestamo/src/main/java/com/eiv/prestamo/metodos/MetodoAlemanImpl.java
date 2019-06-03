5925d4427723aba2d2dd08540f2506bf61c81e10
Diego Cairone <dcairone@eiva.com.ar>
2019-06-03 15:43:03 -0300

    Probando

--- ejercicio-01-prestamo/src/main/java/com/eiv/prestamo/metodos/MetodoAlemanImpl.java
+++ ejercicio-01-prestamo/src/main/java/com/eiv/prestamo/metodos/MetodoAlemanImpl.java
@@ -10,7 +10,7 @@ public class MetodoAlemanImpl implements Metodo {
     public BigDecimal calculoValorCuota(PrestamoDatos prestamoDatos) {
         // TODO Auto-generated method stub
         // Hacemos un cambio
-        return BigDecimal.ZERO;
+        return BigDecimal.TEN;
     }
 
 }
