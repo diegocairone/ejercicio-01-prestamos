bcb79e33c355a77d50dc1dfdff7c9b7293149f83
Diego Cairone <diegocairone@gmail.com>
2019-06-03 15:24:42 -0300

    Cambio desde Github

--- ejercicio-01-prestamo/src/main/java/com/eiv/prestamo/metodos/MetodoAlemanImpl.java
+++ ejercicio-01-prestamo/src/main/java/com/eiv/prestamo/metodos/MetodoAlemanImpl.java
@@ -9,6 +9,7 @@ public class MetodoAlemanImpl implements Metodo {
     @Override
     public BigDecimal calculoValorCuota(PrestamoDatos prestamoDatos) {
         // TODO Auto-generated method stub
+        // Hacemos un cambio
         return BigDecimal.ZERO;
     }
 
